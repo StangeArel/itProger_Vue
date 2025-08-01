766a094cfcc247c4987885fb68179948fc2c4026
Valeriya Stange <valeriya.stange@gmail.com>
2025-08-01 17:14:29 +0200

    🧩feat: Individuelle Fehlermeldungen & dynamische Benutzeranzeige
    ✅ Formulareingabe validiert mit separaten Fehlermeldungen für Name, E-Mail und Passwort
    ✅ Dynamische Anzeige je nach Anzahl der Benutzer: keine, ein, zwei oder mehr
    ✅ Benutzerdaten werden im UI aufgelistet (Name, E-Mail, Passwort)
    🎨 Stile für Benutzerliste und Hinweistexte hinzugefügt

--- vue_itProger/src/App.vue
+++ vue_itProger/src/App.vue
@@ -1,8 +1,129 @@
-<script setup>
-</script>
-
 <template>
+  <form @submit.prevent="sendData">
+    <input type="text" v-model="userName" placeholder="Name *" />
+    <p className="error">{{ errorName }}</p>
+    <input type="password" v-model="userMail" placeholder="Email *" />
+    <p className="error">{{ errorMail }}</p>
+    <input type="email" v-model="userPassword" placeholder="Password *" />
+    <p className="error">{{ errorPassword }}</p>
+    <button @click="sendData()" type="submit">Versenden</button>
+  </form>
+  <p><b>Name:</b> {{ userName }}</p>
+  <p><b>Email:</b> {{ userMail }}</p>
+  <p><b>Password:</b> {{ userPassword }}</p>
+  <p className="users">
+    <u>{{ users }}</u>
+  </p>
+  <div v-if="users.length == 0" className="usersDatenTwo">
+    Derzeit sind keine Benutzer vorhanden 📝
+  </div>
+  <div v-else-if="users.length == 1" className="usersDatenTwo">
+    Users has only 1️⃣ element
+  </div>
+  <div v-else-if="users.length == 2" className="usersDatenTwo">
+    Users has 2️⃣ element
+  </div>
+  <div v-else className="usersDatenTwo">
+    ‼️ Users has more than 2 element ❗️
+  </div>
+  <div v-for="(el, index) in users" :key="index" className="usersDaten">
+    <h3>{{ el.name }}</h3>
+    <p>{{ el.email }}</p>
+    <p>{{ el.password }}</p>
+  </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        users: [],
+        errorName: '',
+        errorMail: '',
+        errorPassword: '',
+        userName: '',
+        userMail: '',
+        userPassword: '',
+      };
+    },
+    methods: {
+      sendData() {
+        if (this.userName === '') {
+          this.errorName = '* Please fill your Name';
+          return;
+        } else if (this.userMail === '') {
+          this.errorMail = '* Please fill your Email';
+          return;
+        } else if (this.userPassword === '') {
+          this.errorPassword = '* Please fill your Password';
+          return;
+        } else {
+        }
+        this.errorName = '';
+        this.errorMail = '';
+        this.errorPassword = '';
+        console.log('🦊 Name:', this.userName);
+        console.log('🦊 Email:', this.userMail);
+        console.log('🦊 Password:', this.userPassword);
+        this.users.push({
+          name: this.userName,
+          email: this.userMail,
+          password: this.userPassword,
+        });
+      },
+    },
+  };
+</script>
+
 <style scoped>
+  form {
+    background-color: greenyellow;
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+    margin: 20px auto;
+    padding: 20px;
+    border-radius: 30px;
+    gap: 10px;
+  }
+
+  p {
+    text-align: center;
+    font-size: 20px;
+    color: red;
+  }
+
+  button {
+    background-color: green;
+    cursor: pointer;
+  }
+
+  .users {
+    font-size: 20px;
+    color: blue;
+    text-align: center;
+  }
+
+  .usersDaten {
+    background-color: lightblue;
+    color: blue;
+    padding: 10px;
+    margin: 10px auto;
+    width: 50%;
+    border-radius: 10px;
+    text-align: center;
+  }
+
+  .usersDatenTwo {
+    background-color: rgba(243, 133, 133, 0.476);
+    color: red;
+    padding: 10px;
+    margin: 10px auto;
+    width: 50%;
+    border-radius: 10px;
+    text-align: center;
+  }
 </style>
