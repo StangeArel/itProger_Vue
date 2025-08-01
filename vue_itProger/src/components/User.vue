ff55ad96e92ff141b61de9613e5d1b3f1b891d4c
Valeriya Stange <valeriya.stange@gmail.com>
2025-08-01 18:49:06 +0200

    📦 User-Komponente erstellt, Formular validiert & Benutzerdaten dynamisch angezeigt

---
+++ vue_itProger/src/components/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="usersDaten">
+        🍎🍐🍊🍒🥦
+        <p>{{ user.name }}</p>
+        <p>{{ user.email }}</p>
+        <p>{{ user.password }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            user: {
+                type: Object,
+                required: true,
+            },
+        },
+    };
+</script>
+
+<style></style>
